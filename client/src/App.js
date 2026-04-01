677ba80b737c1e46816f155d36d243dc7b3e01ef
Hema Kumari K S <159607232+Hema-Kumari15@users.noreply.github.com>
2026-04-01 14:01:08 +0530

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Dashboard />;
+  const token = localStorage.getItem("token");
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {!token ? (
+          <>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        ) : (
+          <Route path="/" element={<Dashboard />} />
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
+export default App;;
