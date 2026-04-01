e0341f9117f7ef15f9aba5eb71453c2ffcb89e3f
Hema Kumari K S <159607232+Hema-Kumari15@users.noreply.github.com>
2026-04-01 12:49:12 +0530

    middleware.js

---
+++ server/middleware/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json("No token");
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decoded.id);
+
+  next();
+};
